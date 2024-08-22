dcf871a34d928a5d18ba634befc4d424c55c6354
jamadrac <rabbikindalo001@gmail.com>
2024-08-22 14:38:16 +0200

    as

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import AppNavigator from "./navigation/AppNavigator";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop:2
+  },
+});
